5074a2088656f23cc5fe66cb8b5ecc876f0aef30
E-Viskhan <viskhan11@gmail.com>
2022-02-15 09:13:33 +0300

    Change design Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
 import Friends from "./Friends/Friends";
+import {User, MessageCircle, Tv, Music, Settings} from "react-feather";
 
 const setActive = ({ isActive }) => isActive ? s.active : '';
 
@@ -10,21 +11,39 @@ const Navbar = (props) => {
             <ul className={s.navList}>
                 <li className={s.item}>
                     <NavLink to='/profile' className={setActive}>
-                        <i className={s.bgPrimaryGradiant + ' ' + s.profileIcon}></i>
+                        <i className={s.bgBlueGradiant}><User/></i>
                         <span>Profile</span>
                     </NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='/dialogs' className={setActive}>Messages</NavLink>
+                    <NavLink to='/dialogs' className={setActive}>
+                        <i className={s.bgOrangeGradiant}><MessageCircle /></i>
+                        <span>Messages</span>
+                    </NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='/news' className={setActive}>News</NavLink>
+                    <NavLink to='/news' className={setActive}>
+                        <i className={s.bgGoldGradiant}>
+                            <Tv/>
+                        </i>
+                        <span>News</span>
+                    </NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='/music' className={setActive}>Music</NavLink>
+                    <NavLink to='/music' className={setActive}>
+                        <i className={s.bgPinkGradiant}>
+                            <Music/>
+                        </i>
+                        <span>Music</span>
+                    </NavLink>
                 </li>
                 <li className={s.item}>
-                    <NavLink to='/settings' className={setActive}>Settings</NavLink>
+                    <NavLink to='/settings' className={setActive}>
+                        <i className={s.bgPrimaryGradiant}>
+                            <Settings/>
+                        </i>
+                        <span>Settings</span>
+                    </NavLink>
                 </li>
             </ul>
             <Friends friends={props.state.Friends}/>
