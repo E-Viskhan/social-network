db2fe028328626e62c89da5d099088fc035414c1
E-Viskhan <viskhan11@gmail.com>
2022-02-19 00:40:37 +0300

    refactoring for reducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,49 +2,35 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 const ADD_MESSAGE = 'ADD-MESSAGE';
 
 const initialState = {
-    dialogs: [
-        {id: 1, name: 'Andrey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img1.png'},
-        {id: 2, name: 'Sergey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img2.png'},
-        {
-            id: 3,
-            name: 'Valeriy',
-            avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img3.png'
-        },
-        {id: 4, name: 'Matvey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img4.png'},
-        {id: 5, name: 'Sasha', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img5.png'},
-    ],
-    messages: [
-        {id: 1, message: 'Hi', isMyMessage: false},
-        {id: 2, message: 'How are you?', isMyMessage: false},
-        {id: 3, message: 'Hello!', isMyMessage: true},
-        {id: 4, message: 'I am fine, and you?', isMyMessage: true},
-        {id: 5, message: 'I am very good, how about our project?', isMyMessage: false},
-        {id: 6, message: 'What is the project?', isMyMessage: true},
-        {id: 7, message: 'Our common project in university', isMyMessage: false},
-        {id: 8, message: 'I was resting, I\'m just starting my part now', isMyMessage: true}
-    ],
-    newMessageText: ''
+    dialogs: [{
+        id: 1, name: 'Andrey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img1.png'
+    }, {id: 2, name: 'Sergey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img2.png'}, {
+        id: 3, name: 'Valeriy', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img3.png'
+    }, {id: 4, name: 'Matvey', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img4.png'}, {
+        id: 5, name: 'Sasha', avatarUrl: 'https://gambolthemes.net/workwise-new/images/resources/m-img5.png'
+    },], messages: [{id: 1, message: 'Hi', isMyMessage: false}, {id: 2, message: 'How are you?', isMyMessage: false}, {
+        id: 3, message: 'Hello!', isMyMessage: true
+    }, {id: 4, message: 'I am fine, and you?', isMyMessage: true}, {
+        id: 5, message: 'I am very good, how about our project?', isMyMessage: false
+    }, {id: 6, message: 'What is the project?', isMyMessage: true}, {
+        id: 7, message: 'Our common project in university', isMyMessage: false
+    }, {id: 8, message: 'I was resting, I\'m just starting my part now', isMyMessage: true}], newMessageText: ''
 };
 
 const dialogsReducer = (state = initialState, action) => {
-
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_TEXT: {
-            const stateCopy = {...state};
-            stateCopy.newMessageText = action.newText;
-            return stateCopy;
-        }
-        case ADD_MESSAGE:
-            const newMessage = {
-                id: 9,
-                message: state.newMessageText,
-                isMyMessage: true
+        case UPDATE_NEW_MESSAGE_TEXT:
+            return {
+                ...state,
+                newMessageText: action.newText
             };
-            const stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = '';
-            return stateCopy;
+        case ADD_MESSAGE:
+            const newMessage = {id: 9, message: state.newMessageText, isMyMessage: true};
+            return {
+                ...state,
+                messages: [...state.messages, newMessage],
+                newMessageText: ''
+            }
         default:
             return state;
     }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,39 +3,32 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const initialState = {
     posts: [
-        {id: 1, message: 'Hi, how are you?', likesCount: 14},
-        {id: 2, message: "it's my first post", likesCount: 15}
+        {id: 1, postText: 'Hi, how are you?', likesCount: 14},
+        {id: 2, postText: "it's my first post", likesCount: 15}
     ],
     newPostText: ''
 };
 
 const profileReducer = (state = initialState, action) => {
-
     switch (action.type) {
-        case UPDATE_NEW_POST_TEXT: {
-            const stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        }
-        case ADD_POST: {
-            let newPost = {
-                id: 5,
-                message: state.newPostText,
-                likesCount: 0
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
             };
-
-            const stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        }
+        case ADD_POST:
+            const newPost = {id: 5, postText: state.newPostText, likesCount: 0};
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         default:
             return state;
     }
 };
 
-export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
-export const addPostActionCreator = () => ({ type: ADD_POST });
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
+export const addPostActionCreator = () => ({type: ADD_POST});
 
 export default profileReducer;
