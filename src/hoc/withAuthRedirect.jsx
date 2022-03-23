017d6e5c73c2de363bb42a48372e71ea34912ed7
E-Viskhan <viskhan11@gmail.com>
2022-03-23 15:08:20 +0300

    turn on redirect when user not auth

--- src/hoc/withAuthRedirect.jsx
+++ src/hoc/withAuthRedirect.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {connect} from "react-redux";
+import {Navigate} from 'react-router-dom';
 
 const mapStateToPropsForRedirect = (state) => ({isAuth: state.auth.isAuth});
 
 const withAuthRedirect = (Component) => {
     const RedirectComponent = (props) => {
-        // if (!props.isAuth) return <Navigate to='/login'/>;
+        if (!props.isAuth) return <Navigate to='/login'/>;
 
         return <Component {...props}/>;
     }
