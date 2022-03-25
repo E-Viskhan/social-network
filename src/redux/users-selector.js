feb304a3f629bfef888157a5a18f645d9434ed43
E-Viskhan <viskhan11@gmail.com>
2022-03-25 14:33:39 +0300

    added selector created with createSelector

--- src/redux/users-selector.js
+++ src/redux/users-selector.js
@@ -1,4 +1,9 @@
+import {createSelector} from "reselect";
+
 export const getUsers = state => state.usersPage.users;
+export const getUsersCleverSelector = createSelector(getUsers, (users) => {
+    return users.filter(u => true);
+});
 export const getPageSize = state => state.usersPage.count;
 export const getCurrentPage = state => state.usersPage.page;
 export const getTotalUsersCount = state => state.usersPage.totalUsersCount;
