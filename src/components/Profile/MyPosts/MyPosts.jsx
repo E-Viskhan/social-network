6249b4a46b8f7cca509a5ac413633ba79229f8ed
E-Viskhan <viskhan11@gmail.com>
2022-03-27 23:21:48 +0300

    changed props for MyPosts component for prevent re-rendering

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,14 @@ import Post from "./Post/Post";
 import React from "react";
 import PostForm from "./PostForm";
 
-const MyPosts = (props) => {
-    const postsElements = props.profilePage.posts.map(p => <Post key={p.id} postText={p.postText} likesCount={p.likesCount}/>);
+const MyPosts = ({ posts, addPost }) => {
+    const postsElements = posts.map(p => <Post key={p.id} postText={p.postText} likesCount={p.likesCount}/>);
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
-            <PostForm addPost={props.addPost}/>
-            <div className={s.posts}>{postsElements}</div>
+            <PostForm addPost={addPost}/>
+            <div>{postsElements}</div>
         </div>
     );
 };
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,7 +2,7 @@ import { addPost } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
-const mapStateToProps = state => ({ profilePage: state.profilePage });
+const mapStateToProps = state => ({ posts: state.profilePage.posts });
 
 const MyPostsContainer = connect(mapStateToProps, { addPost })(MyPosts);
 
