1df6b2b67f9a77c3b4a55625ae605cda241056bc
E-Viskhan <viskhan11@gmail.com>
2022-04-25 17:23:22 +0300

    added passing props & base styles for Preloader

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,5 +1,5 @@
 import preloader from "../../../assets/images/preloader.gif";
 
-const Preloader = (props) => <img src={preloader} alt='preloader'/>;
+const Preloader = props => <img src={preloader} alt='preloader' style={{"display": "block", "margin": "0 auto"}} {...props}/>;
 
 export default Preloader;
